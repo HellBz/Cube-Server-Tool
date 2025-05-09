c0d1734ead745ce1c2553f1d67b9b66ae40407b4
HellBz <stefan-koegler@freenet.de>
2025-05-09 20:21:41 +0200

    Push Latest Gradle Update

--- app/compiled/_start_.sh
+++ app/compiled/_start_.sh
@@ -1,6 +1,6 @@
 
         #!/bin/bash
         echo "Project Name: CubeServerTool"
-        java -jar CubeServerTool.jar
+        java -jar Minecraft-Server-Installer.jar
         read -p "Press any key to continue..."
         
